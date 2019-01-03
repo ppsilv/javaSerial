cd28e16eec1004084f0544c4a9f6b7f1ebed7b64
cs76334 <cs76334@WW7150200097376.br.grupocsu.corp.br>
2019-01-03 18:09:24 -0200

    Arduino serial read sincronization, this code sends an "A" to receive a array of char.

--- src/main/java/serial/Serial.java
+++ src/main/java/serial/Serial.java
@@ -10,6 +10,7 @@ import gnu.io.SerialPortEventListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 /**
  * This version of the TwoWaySerialComm example makes use of the 
@@ -21,7 +22,9 @@ public class Serial
     OutputStream out;
     SerialReader input;
     CommPort commPort;
+    static byte[] buffer_out = new byte[10024];
     static String buffer_string= new String();
+    static int len = 0;
     
     public Serial()
     {
@@ -73,7 +76,7 @@ public class Serial
     public static class SerialReader implements SerialPortEventListener 
     {
         private InputStream in;
-        private byte[] buffer = new byte[10024];
+        byte[] buffer = new byte[10024];
         
         public SerialReader ( InputStream in )
         {
@@ -83,31 +86,20 @@ public class Serial
         public void serialEvent(SerialPortEvent arg0) {
             int data;
 
-            try
-            {
-                int len = 0;
-                while ( ( data = in.read()) > -1 )
-                {
+            try {
+                while ( ( data = in.read()) > -1 ){
                     if ( data == '\n' ) {
+                    	//System.out.println(len);
+                    	buffer_out=Arrays.copyOf(buffer, len);
                         break;
                     }
-                    buffer[len++] = (byte) data;
+                    buffer[len++] = (byte) data;                    
                 }
-                //System.out.print(new String(buffer,0,len));
-                //buffer_string = new String(buffer,0,len);
-                String btr = new String(buffer, 0, len);
-                btr.trim();
-                System.out.print(btr);
-                buffer_string += btr;
-                System.out.println("|"+buffer_string);
-            }
-            catch ( IOException e )
-            {
+            }catch ( IOException e ){
                 e.printStackTrace();
                 System.exit(-1);
             }             
         }
-
     }
 
     /** 
@@ -140,7 +132,7 @@ public class Serial
 
    public String getSensor(){
        try{                
-           this.out.write("yes".getBytes());              
+           this.out.write("A".getBytes());              
        }
        catch ( IOException e ){
            e.printStackTrace();
@@ -154,25 +146,26 @@ public class Serial
        return ("").trim();
    }
 
-    public static void main ( String[] args )
-    {
+    public static void main ( String[] args ){
         Serial test;
-        try
-        {
+        int i;
+        try {
             test = new Serial();
+            Arrays.fill( buffer_out, (byte) '0' );
             test.connect("COM5");
-            buffer_string="-";
-           // buffer_string.trim();
-            test.getSensor();
-            System.out.println("|"+buffer_string+"|");
+            for( i=0; i<150; i++ ) {
+            	len=0;
+                
+	            test.getSensor();
+	            Thread.sleep(150);
+	            String buf = new String(buffer_out);
+            	System.out.println(i+"******************************************************************");
+	            System.out.print( "|" + buf );
+            }
             test.close();
-        }
-        catch ( Exception e )
-        {
+        } catch ( Exception e ) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-
-
 }
